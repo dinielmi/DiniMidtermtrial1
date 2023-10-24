d91cc969b47fb555aab4b0d5db366d0e26337304
unknown <dinielmi912@gmail.com>
2023-10-25 06:57:20 +0700

    Jobsheet 2

---
+++ RedoJobsheet2.java
@@ -0,0 +1,8 @@
+public class RedoJobsheet2 {
+    public static void main(String[] args) {
+        
+        System.out.println("Intinya Jobsheet 2 its talk about github");
+        System.out.println("Colllab di github");
+        System.out.println("dah pokoknya gitu");
+    }
+}
