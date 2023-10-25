7ee847ff91ce2f70fbe009f5b7608ae0e0405117
unknown <dinielmi912@gmail.com>
2023-10-25 08:57:12 +0700

    Jobsheet 6 Leap Year

---
+++ RedoJobsheet6.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class RedoJobsheet6 {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+
+        int tahun;
+
+        System.out.println("Masukkan Tahun");
+        tahun = sc.nextInt();
+
+        if ((tahun % 4) == 0 && (tahun % 100) != 0 || (tahun % 400) == 0){
+            System.out.println("Its Leap Year");
+        } else {
+            System.out.println("Its not Leap Year");
+        }
+
+    }
+}
