bd9585f5ce8a4a1ec7dc6235a900b523b132f1ce
unknown <dinielmi912@gmail.com>
2023-10-25 07:21:04 +0700

    Jobsheet 3

---
+++ RedoJobsheet3.java
@@ -0,0 +1,18 @@
+public class RedoJobsheet3 {
+    public static void main(String[] args) {
+        
+        String salahSatuHobisayaadalah = "Bermain petak umpet";
+        boolean isPandai = true;
+        char jenisKelamin = 'L';
+        byte _umurSayaSekarang = 20 ;
+        double $ipk = 3.24, tinggi = 1.78;
+
+        System.out.println(salahSatuHobisayaadalah);
+        System.out.println("Apakah Pandai? " + isPandai);
+        System.out.println("Jenis Kelmain: " + jenisKelamin);
+        System.out.println("IPK: " + $ipk);
+        System.out.println(String.format(" Saya berumur %s dengan tinghgi badan %s", _umurSayaSekarang + tinggi));
+
+
+    }
+}
